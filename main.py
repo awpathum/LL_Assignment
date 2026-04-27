9b5c18d63c384fe469003c264ef005178b23a010
awpathum <awpathum@gmail.com>
2026-04-27 22:48:53 +0530

    feature :  call api, calculate metrics completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from db_config import get_db
 import logging
+import requests
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -15,5 +17,56 @@ app = FastAPI()
 
 @app.get("/")
 def getYearlyTradingSummary():
-    logger.info("getYearlyTradingSummary")
-    return {"high": "80.8700", "low": "76.0600", "volume": "139457800"}
+
+    response = process_request()
+    return response
+
+
+def process_request():
+    url = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=IBM&apikey=demo"
+    logger.debug(f"Calling API: {url}")
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise exception for bad status codes
+
+        trading_data = response.json()
+
+        summary = calculate_summary(trading_data)
+        logger.info(summary)
+
+        return summary
+
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error calling Alpha Vantage API: {str(e)}")
+        return {
+            "error": f"Failed to fetch trading data: {str(e)}",
+        }
+
+
+def calculate_summary(trading_data):
+    trading_data_by_day = trading_data.get("Monthly Time Series", {})
+
+    # print(trading_data_by_day)
+
+    highs = []
+    lows = []
+    volumes = []
+
+    for data_point in trading_data_by_day.values():
+        highs.append(float(data_point["2. high"]))
+        lows.append(float(data_point["3. low"]))
+        volumes.append(int(data_point["5. volume"]))
+
+    logger.info(highs)
+    logger.info(lows)
+    logger.info(volumes)
+
+    return {
+        "highest": max(highs) if highs else 0,
+        "lowest": min(lows) if lows else 0,
+        "total_volumn": sum(volumes) if volumes else 0,
+    }
+
+
+# https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=IBM&apikey=demo
